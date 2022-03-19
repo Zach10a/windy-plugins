8d094446030aaafbce8cb471d0a34fbbbf034afe
Zach10a <zachtait84@outlook.com>
2022-03-19 13:59:48 +0000

    Make ICAO list for API

--- dist/plugin.js
+++ dist/plugin.js
@@ -34,29 +34,40 @@ function () {
   this.onopen = function () {
     console.log('I am being opened');
     var center = map.getCenter();
-    var icao = 'EGDM';
-    var icao_list = [EGDM];
+    var icao_list = ["EGDM", "EGYE", "EGUB", "EGVN", "EGXC", "EGWC", "EGYD", "EGXE", "EGQS", "EGYM", "EGWU", "EGVO", "EGXP", "EGOS", "EGSY", "EGOV", "EGXW", "WOTG", "EGXT", "EGOW"];
+    var icao_list_string = "";
+
+    for (var i = 0; i < icao_list.length - 1; i++) {
+      if (i < icao_list.length) {
+        icao_list_string += icao_list[i] + ",";
+      } else {
+        icao_list_string += icao_list[i];
+      }
+    }
 
     xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         console.log(xhttp.responseText);
-        console.log(xhttp);
 
         var _METAR_data = JSON.parse(xhttp.responseText);
 
-        console.log(_METAR_data["data"][0]);
+        console.log(_METAR_data["data"][0][0]);
+        console.log(icao_list_string);
+        return _METAR_data;
       }
     };
 
-    var web_string = "https://api.checkwx.com/metar/".concat(icao, "/decoded");
+    var web_string = "https://api.checkwx.com/metar/".concat(icao_list_string, "/decoded");
     xhttp.open("GET", web_string, true);
     xhttp.setRequestHeader('X-API-Key', '6b8ce0d9f2ee488a8e5f91ce58');
     xhttp.send();
+    var METAR_data = METAR_data;
+    console.log(icao_list_string);
 
     if (popup) {
       popup.setLatLng(center);
     } else {
-      popup = L.popup().setLatLng(center).setContent(METAR_data.data).openOn(map);
+      popup = L.popup().setLatLng(center).setContent(METAR_data["data"][0]).openOn(map);
     }
   };
 
